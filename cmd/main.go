49b0b408eb5b1e7510b086fb7562876362b3f0ab
Wachirapong Prasertwong <ni@oozou.com>
2025-03-16 19:42:19 +0700

    add CORS middlewares

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/wp-wachi/stripe-payment-golang/config"
+	"github.com/wp-wachi/stripe-payment-golang/middlewares"
 	"github.com/wp-wachi/stripe-payment-golang/routes"
 
 	"github.com/gin-gonic/gin"
@@ -14,6 +15,10 @@ func main() {
 	// database.ConnectDB()
 
 	r := gin.Default()
+
+	// Apply CORS Middleware
+	r.Use(middlewares.CORSMiddleware())
+
 	routes.RegisterRoutes(r)
 
 	log.Println("Server running on port 8080")
